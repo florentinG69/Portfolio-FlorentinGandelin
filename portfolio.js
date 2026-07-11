3ed83e388c35dff3f6fd75834c5fe630e2ceba6b
Flo <florentingandelin69@gmail.com>
2026-07-11 07:50:42 +0200

    commit

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+const header = document.getElementById("header");
+
+window.addEventListener("scroll",() => {
+    if(window.scrollY > 0){
+        header.classList.add("header-scroll");     
+    }else(
+        header.classList.remove("header-scroll")
+    )
+
+})
